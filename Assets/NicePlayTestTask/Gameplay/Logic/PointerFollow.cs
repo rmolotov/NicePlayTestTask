a61a324b2b5cc138e0ae17d747d571a399da77a3
Roman Molotov <r.molotov@yahoo.com>
2023-12-04 16:21:44 +0400

    proto Drag n Drop mechanic

---
+++ Assets/NicePlayTestTask/Gameplay/Logic/PointerFollow.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using Zenject;
+using DG.Tweening;
+using NicePlayTestTask.Services.Input;
+
+namespace NicePlayTestTask.Gameplay.Logic
+{
+    public class PointerFollow : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rigidbodyComponent;
+        [SerializeField] private float speed = 0.1f;
+        
+        private IInputService _inputService;
+
+        private Sequence _moveSequence;
+
+        [Inject]
+        private void Construct(IInputService inputService)
+        {
+            _inputService = inputService;
+        }
+
+        private void Start()
+        {
+            _inputService.Drag += BeginDrag;
+        }
+
+        public void BeginDrag()
+        {
+            _inputService.Drag -= BeginDrag;
+            _inputService.Drop += EndDrag;
+            
+            _inputService.PointerPositionChanged += UpdatePosition;
+            rigidbodyComponent.gravityScale = 0;
+        }
+
+        public void EndDrag()
+        {
+            _inputService.Drop -= EndDrag;
+            _inputService.Drag += BeginDrag;
+            
+            _inputService.PointerPositionChanged -= UpdatePosition;
+            rigidbodyComponent.gravityScale = 1;
+        }
+
+        private void UpdatePosition(Vector2 pointerPosition)
+        {
+            _moveSequence?.Complete();
+            _moveSequence = DOTween
+                .Sequence()
+                .Append(
+                    rigidbodyComponent.DOMove(
+                        Camera.main.ScreenToWorldPoint(pointerPosition),
+                        1f / speed)
+                )
+                .Play();
+        }
+    }
+}
