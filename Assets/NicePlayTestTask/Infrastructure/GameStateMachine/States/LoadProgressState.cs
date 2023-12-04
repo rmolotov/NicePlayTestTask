138092b03fc88d2766040ed35d0b4dd13b543545
Roman Molotov <r.molotov@yahoo.com>
2023-12-04 11:17:43 +0400

    load or init new progress in LoadProgressState

--- Assets/NicePlayTestTask/Infrastructure/GameStateMachine/States/LoadProgressState.cs
+++ Assets/NicePlayTestTask/Infrastructure/GameStateMachine/States/LoadProgressState.cs
@@ -1,18 +1,31 @@
+using System.Collections.Generic;
+using NicePlayTestTask.Data;
 using NicePlayTestTask.Infrastructure.GameStateMachine.States.Interfaces;
+using NicePlayTestTask.Services.PersistentData;
+using NicePlayTestTask.Services.SaveLoad;
 
 namespace NicePlayTestTask.Infrastructure.GameStateMachine.States
 {
     public class LoadProgressState : IState
     {
         private readonly GameStateMachine _stateMachine;
+        private readonly IPersistentDataService _progressService;
+        private readonly ISaveLoadService _saveLoadProgressService;
 
-        public LoadProgressState(GameStateMachine stateMachine)
+        public LoadProgressState(
+            GameStateMachine stateMachine, 
+            IPersistentDataService progressService, 
+            ISaveLoadService saveLoadService)
         {
             _stateMachine = stateMachine;
+            _progressService = progressService;
+            _saveLoadProgressService = saveLoadService;
         }
         
         public void Enter()
         {
+            LoadProgressOrInitNew();
+
             _stateMachine.Enter<LoadMetaState>();
         }
 
@@ -20,5 +33,21 @@ namespace NicePlayTestTask.Infrastructure.GameStateMachine.States
         {
             
         }
+        
+        private async void LoadProgressOrInitNew()
+        {
+            _progressService.Progress = 
+                await _saveLoadProgressService.LoadProgress() 
+                ?? NewProgress();
+        }
+        
+        private PlayerProgressData NewProgress() =>
+            new()
+            {
+                Score = 0,
+                BestDish = new Dictionary<string, int>(),
+                LastDish = new Dictionary<string, int>(),
+                UnlockedRecipes = new HashSet<string>()
+            };
     }
 }
