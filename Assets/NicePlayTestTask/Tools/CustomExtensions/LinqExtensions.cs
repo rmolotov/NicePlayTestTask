85d4ca2e77677c667c973ae323164a2491c4b780
Roman Molotov <r.molotov@yahoo.com>
2023-12-06 17:17:59 +0400

    add linq and func extensionss

---
+++ Assets/NicePlayTestTask/Tools/CustomExtensions/LinqExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NicePlayTestTask.Tools.CustomExtensions
+{
+    public static class LinqExtensions
+    {
+        public static T MaxBy<T, R>(this IEnumerable<T> en, Func<T, R> evaluate) where R : IComparable<R> =>
+            en.Select(t => new Tuple<T, R>(t, evaluate(t)))
+                .Aggregate((max, next) => next.Item2.CompareTo(max.Item2) > 0 ? next : max).Item1;
+
+        public static T MinBy<T, R>(this IEnumerable<T> en, Func<T, R> evaluate) where R : IComparable<R> =>
+            en.Select(t => new Tuple<T, R>(t, evaluate(t)))
+                .Aggregate((max, next) => next.Item2.CompareTo(max.Item2) < 0 ? next : max).Item1;
+    }
+}
